d41a0964f7dcd70effa2e2ab054c7165cfa56aa4
pascal918 <kai.zhang@ensta-bretagne.org>
2021-05-10 10:05:35 +0200

    [w] update setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+
+
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "numpy~=1.19.2",
+    "tensorflow~=2.4.1",
+    "matplotlib~=3.4.1",
+    "pandas~=1.2.4", 
+    "sklearn",
+    "scipy~=1.6.3",
+    "seaborn~=0.11.1", 
+    "ipython~=5.7",
+    "notebook~=6.3.0",
+    "Werkzeug~=1.0.1",
+
+
+]
+
+
+setup(
+    name="basic",
+    version="0.0.1",
+    groupid="free.basic",
+    description="test",
+    author="kzhang",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8'
+    ],
+    install_requires=install_requires,
+    packages=find_packages("src"),
+    zip_safe=False,
+
+    package_dir={"": "src"},
+)
+
+
